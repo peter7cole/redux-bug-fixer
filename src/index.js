8e851cf8a0506ce620bcfa03c7b947da32ab15bc
‘peter-robert-cole’ <petercole3@gmail.com>
2020-10-15 23:14:52 -0700

    immer practice

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+// console.log('Hello World!');
+import { produce } from 'immer';
+
+let book = { title: 'Happy Father' };
+
+function publish(book) {
+	return produce(book, draftBook => {
+		// mutating code goes here!
+		draftBook.isPublished = true;
+	});
+}
+
+let updatedBook = publish(book);
+
+console.log('book', book);
+console.log('updatedBook', updatedBook);
